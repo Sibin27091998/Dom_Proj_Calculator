73636dad688fa24b87120f5de0e7946487b61232
Sibin B A <sibinba98@gmail.com>
2023-05-12 17:27:25 -0700

    first commit

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function displayData(n){
+    result.value=result.value+n
+}
+function evaluateOper(){
+    result.value=eval(result.value)
+}
+function allClear(){
+    result.value=""
+}
+
+
+
+//slice
+//two arguemnts
+//1.starting index and  2.ending index
+function backspace(){
+    result.value=result.value.slice(0,-1)
+}
+//result.valuil nthuvanooo content ollee athinee access cheythit orr element slicee cheythit kalanjit nereee result.valuil
+//thanne store cheyth vekkumm
